1d8a4c6b595a180529c662d6af72038bcabe0e01
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-01-27 11:25:54 -0500

    added models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,33 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 // connect to the wikistack database
 mongoose.connect('mongodb://localhost/wikistack');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
+
+// define the page Schema
+var pageSchema = new Schema({
+  title:    String,
+  urlTitle: String,
+  content:  String,
+  date:     {type: Date, default: Date.now},
+  status:   String,
+  author:   {type: mongoose.Schema.Types.ObjectId, ref:'User'}
+});
+
+// define the user Schema
+var userSchema = new Schema({
+  name:   String,
+  email:  String
+});
+
+// compile Schemas into Models
+var Page = mongoose.model('Page', pageSchema);
+var User = mongoose.model('User', userSchema);
+
+module.exports = {
+  Page: Page,
+  User: User
+};
